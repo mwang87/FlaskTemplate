cd5799f7f01f72f7dc54ab1e4ccc58fb932fac16
mingxun <mingxun@ubuntu.(none)>
2016-03-07 04:54:25 -0800

    updating files to make it heroku compatible

--- webserver.py
+++ webserver.py
@@ -16,5 +16,6 @@ def testapi():
     return json.dumps(return_obj)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
