a1b318c958fb687eb9b2c8872011ca2f28eceec6
Ming Wang <mwang87@gmail.com>
2019-01-12 18:53:19 -0800

    Updating template to include db stubs and put in docker

---
+++ views.py
@@ -0,0 +1,21 @@
+# views.py
+from flask import abort, jsonify, render_template, request, redirect, url_for, send_file, make_response
+
+from app import app
+from models import *
+
+import os
+import csv
+import json
+import uuid
+import requests
+
+@app.route('/', methods=['GET'])
+def renderhomepage():
+    return render_template('index.html')
+
+@app.route('/heartbeat', methods=['GET'])
+def testapi():
+    return_obj = {}
+    return_obj["status"] = "success"
+    return json.dumps(return_obj)
